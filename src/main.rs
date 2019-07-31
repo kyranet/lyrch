6042c43de35f9b7d4ad989ac61a9b8378daf0c75
kyranet <kyradiscord@gmail.com>
2019-07-31 23:49:12 +0200

    style: Ran `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate dotenv;
 extern crate serenity;
 
-use std::env;
 use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use std::env;
 
 struct Handler;
 
@@ -15,7 +15,11 @@ impl EventHandler for Handler {
             let response = msg.channel_id.say(&context, "Ping...");
             if let Ok(reply) = response {
                 let latency = (reply.timestamp - msg.timestamp).num_milliseconds();
-                msg.channel_id.edit_message(&context, reply, |e| e.content(format!("Pong! Took: {}ms!", latency))).ok();
+                msg.channel_id
+                    .edit_message(&context, reply, |e| {
+                        e.content(format!("Pong! Took: {}ms!", latency))
+                    })
+                    .ok();
             }
         } else if msg.content == "!messageme" {
             // If the `utils`-feature is enabled, then model structs will
@@ -47,8 +51,7 @@ fn main() {
     dotenv::dotenv().unwrap();
 
     // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let mut client = Client::new(&token, Handler).expect("Err creating client");
 
     if let Err(why) = client.start() {
