eb223987dfc5077ab0b0b7c000d1c85925ec6788
kyraNET <kyradiscord@gmail.com>
2019-08-18 17:54:49 +0200

    macros: Optimized send_message's performance

--- src/lib/util/macros.rs
+++ src/lib/util/macros.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! send_message {
-    ($ctx:expr, $msg:expr, $($arg:tt)*) => {
+    ($ctx:expr, $msg:expr, $fmt:expr) => {
         {
             use crate::lib::core::EditableMessages;
 
@@ -11,10 +11,10 @@ macro_rules! send_message {
 
             if let Some(older) = editable_messages.get(&$msg.id) {
                 $msg.channel_id.edit_message(&$ctx, older, |e| {
-                    e.content(format!($($arg)*))
+                    e.content($fmt)
                 })
             } else {
-                match $msg.channel_id.say(&$ctx, format!($($arg)*)) {
+                match $msg.channel_id.say(&$ctx, $fmt) {
                     Ok(new_message) => {
                         editable_messages.insert($msg.id, new_message.id);
                         Ok(new_message)
@@ -23,5 +23,8 @@ macro_rules! send_message {
                 }
             }
         }
+    };
+    ($ctx:expr, $msg:expr, $fmt:expr, $($args:tt)+) => {
+        send_message!($ctx, $msg, format!($fmt, format!($fmt, $($args)*)))
     };
 }
